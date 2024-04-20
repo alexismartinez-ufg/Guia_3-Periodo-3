18b2e98ed0327d833e4bac8c40e820e35dfb2b20
ia.alexismartinez <ia.alexismartinez@ufg.edu.sv>
2024-04-20 11:18:56 -0600

    Add project ASP.NETCORE 8 MVC

---
+++ Ejercicio_1/Controllers/ContactoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio_1.Controllers
+{
+    public class ContactoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
